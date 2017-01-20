113e73138c5b89f73828d067e51275a3fa137e5e
Davide Bontempelli <davide.bonte@gmail.com>
2017-01-20 15:00:14 +0100

    add more ES6 code

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@
 var botBuilder = require('claudia-bot-builder');
 
 module.exports = botBuilder(function (request, originalApiRequest) {
-  var prettyResponse = JSON.stringify(request, null, 1);
+  var prettyResponse = 'Here is your request: \n ' + JSON.stringify(request, null, 1);
   return prettyResponse;
 });
--- src/bot.source.js
+++ src/bot.source.js
@@ -1,6 +1,6 @@
 const botBuilder = require('claudia-bot-builder')
 
 module.exports = botBuilder((request, originalApiRequest) => {
-  const prettyResponse = JSON.stringify(request,null, 1)
+  const prettyResponse = `Here is your request: \n ${JSON.stringify(request,null, 1)}`
   return prettyResponse
 })
